c138b925bce3cc0b2b3e817039b5dffec8a33eaa
AdamAndrei <adam.andrei154@gmail.com>
2026-03-08 16:09:56 +0200

    Add build/run configuration for lists_lesson

---
+++ lists_lesson/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "include/linked-lists/demos/SimpleLinkedListDemo.h"
+#include "include/linked-lists/demos/StackDemo.h"
+#include "include/linked-lists/demos/SelfOrganizedListDemo.h"
+#include "include/linked-lists/queue/Queue.h"
+#include "include/linked-lists/queue/SortedPriorityQueue.h"
+#include "include/linked-lists/queue/PriorityQueue.h"
+
+int main()
+{
+    demoSimpleLinkedList();
+    demoStack();
+    demoSelfOrganizedList();
+    return 0;
+}
